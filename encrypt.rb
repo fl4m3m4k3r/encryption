d56fb7e615e987e0d7ce9addc2910c207ac8c1d9
flamemaker <fl4m3m4k3r@gmail.com>
2021-01-19 21:30:44 +0300

    Added sha2 algorithm

--- encrypt.rb
+++ encrypt.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 #
 # Программа для шифрования слова с помощью механизма MD5 или SHA1
-
 if Gem.win_platform?
   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
   Encoding.default_internal = __ENCODING__
@@ -19,10 +18,11 @@ string = STDIN.gets.chomp
 puts 'Каким способом зашифровать:'
 puts '1. MD5'
 puts '2. SHA1'
+puts '3. SHA2'
 encrypt_method = STDIN.gets.to_i
 
-until encrypt_method.between?(1, 2)
-  puts 'Выберите 1 или 2'
+until encrypt_method.between?(1, 3)
+  puts 'Выберите число от 1 до 3'
   encrypt_method = STDIN.gets.to_i
 end
 
@@ -32,4 +32,5 @@ puts 'Вот что получилось:'
 case encrypt_method
 when 1 then puts Digest::MD5.hexdigest(string)
 when 2 then puts Digest::SHA1.hexdigest(string)
+when 3 then puts Digest::SHA2.hexdigest(string)
 end
